c5417811346eb06811edeacf1e5f4c9f18890609
leejihyun <leejudy1017@naver.com>
2021-07-27 15:04:57 +0900

    FlightSearch : Calendar (limit)

--- app/src/main/java/com/example/carry_stray_dogs/FlightSearchActivity.kt
+++ app/src/main/java/com/example/carry_stray_dogs/FlightSearchActivity.kt
@@ -2,32 +2,46 @@ package com.example.carry_stray_dogs
 
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_flightsearch.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 
 class FlightSearchActivity : AppCompatActivity(){
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flightsearch)
         //액션바 제거
-        var actionBar : ActionBar?
-        actionBar = supportActionBar
+        var actionBar : ActionBar? = supportActionBar
         actionBar?.hide()
+
+
+        //오늘날짜로 default 설정
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val formatted = current.format(formatter)
+        start_date.text = formatted.split("-")[0]+"."+formatted.split("-")[1]+"."+formatted.split("-")[2]
+        end_date.text = formatted.split("-")[0]+"."+formatted.split("-")[1]+"."+formatted.split("-")[2]
+        val minDate = Calendar.getInstance()
+        minDate.set(Integer.parseInt(formatted.split("-")[0]),Integer.parseInt(formatted.split("-")[1]),Integer.parseInt(formatted.split("-")[2]))
 
         swapBtn.setOnClickListener {
            val start = startText.text.toString()
             val end = endText.text.toString()
 
-            if(start.equals("출발지")){
+            if(start == "출발지"){
                 Toast.makeText(applicationContext, "출발지를 설정해주세요.", Toast.LENGTH_SHORT).show()
             }
-            else if(end.equals("도착지")){
+            else if(end == "도착지"){
                 Toast.makeText(applicationContext, "도착지를 설정해주세요.", Toast.LENGTH_SHORT).show()
             }
             else{
@@ -53,8 +67,9 @@ class FlightSearchActivity : AppCompatActivity(){
             var month = calendar.get(Calendar.MONTH)
             var day = calendar.get(Calendar.DAY_OF_MONTH)
 
-            var listener = DatePickerDialog.OnDateSetListener{_,i,i2,i3 -> start_date.setText(""+i+"."+i2+"."+i3)}
+            var listener = DatePickerDialog.OnDateSetListener{_,i,i2,i3 -> start_date.text = ""+i+"."+i2+"."+i3 }
             var picker = DatePickerDialog(this,listener,year,month,day)
+            picker.getDatePicker().setMinDate(minDate.getTime().getTime())
             picker.show()
         }
 
@@ -63,9 +78,10 @@ class FlightSearchActivity : AppCompatActivity(){
             var year = calendar.get(Calendar.YEAR)
             var month = calendar.get(Calendar.MONTH)
             var day = calendar.get(Calendar.DAY_OF_MONTH)
-
-            var listener = DatePickerDialog.OnDateSetListener{_,i,i2,i3 -> end_date.setText(""+i+"."+i2+"."+i3)}
+            var listener = DatePickerDialog.OnDateSetListener{_,i,i2,i3 -> end_date.text = ""+i+"."+i2+"."+i3 }
             var picker = DatePickerDialog(this,listener,year,month,day)
+            minDate.set(Integer.parseInt(start_date.text.split(".")[0]),Integer.parseInt(start_date.text.split(".")[1]),Integer.parseInt(start_date.text.split(".")[2]))
+            picker.getDatePicker().setMinDate(minDate.getTime().getTime())
             picker.show()
         }
 

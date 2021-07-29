3d30c582d6f502d90de0daff76879b10e9ed901f
leejihyun <leejudy1017@naver.com>
2021-07-29 20:09:08 +0900

    Searchresult : Dynamic button (2)

--- app/src/main/java/com/example/carry_stray_dogs/SearchResultActivity.kt
+++ app/src/main/java/com/example/carry_stray_dogs/SearchResultActivity.kt
@@ -1,13 +1,16 @@
 package com.example.carry_stray_dogs
 
+import android.R.attr.left
 import android.content.Intent
-import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.view.Gravity
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_flightsearch.*
 import kotlinx.android.synthetic.main.activity_flightsearch.endText
 import kotlinx.android.synthetic.main.activity_flightsearch.startText
@@ -16,9 +19,10 @@ import kotlinx.android.synthetic.main.activity_searchresult.*
 
 class SearchResultActivity : AppCompatActivity() {
 
-    var tagButton : Button? =null
-    var dynamicButton : Button? =null
+    var dynamicButton : ImageButton? =null
+    var circleView : ImageView? =null
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searchresult)
@@ -70,19 +74,56 @@ class SearchResultActivity : AppCompatActivity() {
 
         //Adapt Dog Info
         val buttonview = findViewById<LinearLayout>(R.id.Info)
+        /*
         for(i: Int in 1..10) {
             dynamicButton = Button(this)
+            circleView = ImageView(this)
             val layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 changeDP(130)
             )  //width height
+
+            //circle crop
+            Glide.with(this).load(R.drawable.dog).circleCrop().into(circleView!!)
+
             dynamicButton!!.layoutParams = layoutParams
-            layoutParams.setMargins(
-                changeDP(0),
-                changeDP(0),
-                changeDP(0),
-                0
-            ) //left top right bottom
+
+            //button custom
+            dynamicButton!!.background = ContextCompat.getDrawable(this, R.drawable.searchresult_btn_custom)
+            dynamicButton!!.text = "PETCH !!!!!"
+
+            dynamicButton!!.setCompoundDrawablesWithIntrinsicBounds(circleView!!.drawable, null, null, null)
+
+            if(circleView!!.drawable==null){
+                Log.i("null? ","yes")
+            }
+            else{
+                Log.i("null? ","no")
+                Log.i("null? ",circleView!!.drawable.toString())
+            }
+
+            layoutParams.setMargins(changeDP(0), changeDP(10), changeDP(0), changeDP(0)) //left top right bottom
+            buttonview.addView(dynamicButton)
+        }
+       */
+        for(i: Int in 1..10) {
+            dynamicButton = ImageButton(this)
+            circleView = ImageView(this)
+
+            val layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                changeDP(130)
+            )  //width height
+
+            //circle crop
+            Glide.with(this).load(R.drawable.dog).circleCrop().into(dynamicButton!!)
+
+            dynamicButton!!.layoutParams = layoutParams
+
+            //button custom
+            dynamicButton!!.background = ContextCompat.getDrawable(this, R.drawable.searchresult_btn_custom)
+            layoutParams.setMargins(changeDP(20), changeDP(10), changeDP(0), changeDP(20)) //left top right bottom
+            layoutParams.gravity = Gravity.LEFT or Gravity.CENTER_HORIZONTAL
             buttonview.addView(dynamicButton)
         }
     }
